5f2d6dfc7a0894afeecc2f5156ced9851d27cec8
Sergey Ryadovoy <ryadovoysergey@gmail.com>
2022-12-16 12:47:18 +0600

    Тест. Добавлены переменные a и b

--- Converter.java
+++ Converter.java
@@ -1,5 +1,5 @@
 class Converter{
     public static void main(String[] args) {
-        
+        int a;
     }
 }
--- Praktikum.java
+++ Praktikum.java
@@ -1,5 +1,5 @@
 class Praktikum{
     public static void main(String[] args) {
-        
+        int b;
     }
 }
